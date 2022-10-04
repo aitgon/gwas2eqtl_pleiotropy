ebcab67b18c4da28b6d5a853b28d543c5b4ba6ef
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2022-10-04 09:32:41 +0200

    ENH remove dots from col names

--- scripts/cmpt_public_web_files.py
+++ scripts/cmpt_public_web_files.py
@@ -7,8 +7,7 @@ import pandas
 import pathlib
 import sys
 
-
-#%%
+# %%
 help_cmd_str = "todo"
 try:
     db_sqlite = sys.argv[1]
@@ -25,15 +24,16 @@ except IndexError:
 outdir_path = os.path.dirname(coloc_tsv_gz_path)
 pathlib.Path(outdir_path).mkdir(parents=True, exist_ok=True)
 
-#%% Insert into coloc
-engine = create_engine("sqlite:///{}".format(db_sqlite), echo = False)
+# %% Insert into coloc
+engine = create_engine("sqlite:///{}".format(db_sqlite), echo=False)
 meta.create_all(engine)
 
-#%%
+# %%
 with engine.connect() as con:
     for chrom in range(1, 23):  # loop over chroms to avoid memory problems
         # print(chrom)
-        sel_cols = [coloc, gwas_annot_tbl.c.gwas_trait, gwas_annot_tbl.c.gwas_class, ensg2symbol_tbl.c.symbol, rsid2cytoband_tbl.c.cytoband]
+        sel_cols = [coloc, gwas_annot_tbl.c.gwas_trait, gwas_annot_tbl.c.gwas_class, ensg2symbol_tbl.c.symbol,
+                    rsid2cytoband_tbl.c.cytoband]
         sel = select(sel_cols).distinct()
         sel = sel.where(coloc.c.gwas_id == gwas_annot_tbl.c.gwas_id)
         sel = sel.where(coloc.c.egene == ensg2symbol_tbl.c.gene_id)
@@ -48,34 +48,37 @@ with engine.connect() as con:
         coloc_df = pandas.DataFrame(result_lst)
         coloc_df.drop(['id'], inplace=True, axis=1)
         coloc_df.rename({'symbol': 'egene_symbol'}, axis=1, inplace=True)
-        coloc_cols = []
-        coloc_cols.append('chrom')
-        coloc_cols.append('pos')
-        coloc_cols.append('cytoband')
-        coloc_cols.append('rsid')
-        coloc_cols.append('ref')
-        coloc_cols.append('alt')
-        coloc_cols.append('gwas_trait')
-        coloc_cols.append('gwas_class')
-        coloc_cols.append('gwas_beta')
-        coloc_cols.append('egene_symbol')
-        coloc_cols.append('eqtl_beta')
-        coloc_cols.append('eqtl_id')
-        coloc_cols.append('egene')
-        coloc_cols.append('gwas_id')
-        coloc_cols.append('gwas_pval')
-        coloc_cols.append('eqtl_pval')
-        coloc_cols.append('PP.H4.abf')
-        coloc_cols.append('SNP.PP.H4')
-        coloc_cols.append('PP.H3.abf')
-        coloc_cols.append('PP.H2.abf')
-        coloc_cols.append('PP.H1.abf')
-        coloc_cols.append('PP.H0.abf')
-        coloc_cols.append('coloc_region')
-        coloc_cols.append('coloc_lead_pos')
-        coloc_cols.append('coloc_lead_rsid')
-        coloc_cols.append('nsnps')
-        coloc_df = coloc_df[coloc_cols]
+        col_name1_lst = []
+        col_name1_lst.append('chrom')
+        col_name1_lst.append('pos')
+        col_name1_lst.append('cytoband')
+        col_name1_lst.append('rsid')
+        col_name1_lst.append('ref')
+        col_name1_lst.append('alt')
+        col_name1_lst.append('gwas_trait')
+        col_name1_lst.append('gwas_class')
+        col_name1_lst.append('gwas_beta')
+        col_name1_lst.append('egene_symbol')
+        col_name1_lst.append('eqtl_beta')
+        col_name1_lst.append('eqtl_id')
+        col_name1_lst.append('egene')
+        col_name1_lst.append('gwas_id')
+        col_name1_lst.append('gwas_pval')
+        col_name1_lst.append('eqtl_pval')
+        col_name1_lst.append('PP.H4.abf')
+        col_name1_lst.append('SNP.PP.H4')
+        col_name1_lst.append('PP.H3.abf')
+        col_name1_lst.append('PP.H2.abf')
+        col_name1_lst.append('PP.H1.abf')
+        col_name1_lst.append('PP.H0.abf')
+        col_name1_lst.append('coloc_region')
+        col_name1_lst.append('coloc_lead_pos')
+        col_name1_lst.append('coloc_lead_rsid')
+        col_name1_lst.append('nsnps')
+        coloc_df = coloc_df[col_name1_lst]
+        # remove dots
+        col_name2_lst = [col.replace('.', '_') for col in col_name1_lst]
+        coloc_df.rename(dict(zip(col_name1_lst, col_name2_lst)), axis=1, inplace=True)
         if chrom == 1:  # write new
             coloc_df.to_csv(coloc_tsv_gz_path, sep='\t', index=False, header=True)
         elif chrom > 1:  # append
