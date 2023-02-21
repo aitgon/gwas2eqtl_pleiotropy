c3aa20381357315751f32ef63dd6ce0339b25f31
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2023-02-21 21:25:05 +0100

    ENH

--- scripts/cmpt_ucsc_tracks_eqtl_inter_bed.py
+++ scripts/cmpt_ucsc_tracks_eqtl_inter_bed.py
@@ -23,35 +23,35 @@ except IndexError:
 
 # outdir_path = os.path.dirname(track_tsv)
 pathlib.Path(outdir_path).mkdir(parents=True, exist_ok=True)
-track_data_hub_dir = os.path.join(outdir_path, "ucsc_hub")
-pathlib.Path(track_data_hub_dir).mkdir(parents=True, exist_ok=True)
-
-hub_txt_path = os.path.join(track_data_hub_dir, "hub.txt")
-hub_txt_str = """hub gwas2eqtl
-shortLabel gwas2eqtl
-longLabel Colocalization analysis of IEU OpenGWAS and EBI eQTL Catalogue  
-genomesFile genomes.txt
-email aitor.gonzalez@univ-amu.fr
-descriptionUrl https://gwas2eqtl.tagc.univ-amu.fr/gwas2eqtl/"""
-with open(hub_txt_path, 'w') as fout:
-    fout.write(hub_txt_str)
-
-genomes_txt_path = os.path.join(track_data_hub_dir, "genomes.txt")
-genomes_txt_str = """genome hg38
-trackDb hg38/trackDb.txt"""
-with open(genomes_txt_path, 'w') as fout:
-    fout.write(genomes_txt_str)
-
-# hub_txt_path = os.path.join(track_data_hub_dir, "hg38/trackDb.txt")
-# hub_txt_str = """track footprint_scores
-# compositeTrack on
-# shortLabel coloc intersection
-# longLabel coloc footprint scores
-# visibility full
-# priority 3
-# type bed"""
-# with open(hub_txt_str, 'w') as fout:
-#     fout.write(hub_txt_path)
+# track_data_hub_dir = os.path.join(outdir_path, "ucsc_hub")
+# pathlib.Path(track_data_hub_dir).mkdir(parents=True, exist_ok=True)
+#
+# hub_txt_path = os.path.join(track_data_hub_dir, "hub.txt")
+# hub_txt_str = """hub gwas2eqtl
+# shortLabel gwas2eqtl
+# longLabel Colocalization analysis of IEU OpenGWAS and EBI eQTL Catalogue
+# genomesFile genomes.txt
+# email aitor.gonzalez@univ-amu.fr
+# descriptionUrl https://gwas2eqtl.tagc.univ-amu.fr/gwas2eqtl/"""
+# with open(hub_txt_path, 'w') as fout:
+#     fout.write(hub_txt_str)
+#
+# genomes_txt_path = os.path.join(track_data_hub_dir, "genomes.txt")
+# genomes_txt_str = """genome hg38
+# trackDb hg38/trackDb.txt"""
+# with open(genomes_txt_path, 'w') as fout:
+#     fout.write(genomes_txt_str)
+#
+# # hub_txt_path = os.path.join(track_data_hub_dir, "hg38/trackDb.txt")
+# # hub_txt_str = """track footprint_scores
+# # compositeTrack on
+# # shortLabel coloc intersection
+# # longLabel coloc footprint scores
+# # visibility full
+# # priority 3
+# # type bed"""
+# # with open(hub_txt_str, 'w') as fout:
+# #     fout.write(hub_txt_path)
 
 
 #%%
@@ -96,21 +96,23 @@ df2['color'] = '#008000'
 df2.loc[df['eqtl_beta'] < 0, 'color'] = '#FF0000'
 df2['name'] = df2['sourceName'] + '/' + df2['targetName'] + '/' + df2['exp']
 
-for etissue_category_term in sorted(df2['etissue_category_term'].unique()):
-    print(etissue_category_term)
-    cell_df = df2.loc[df2['etissue_category_term'] == etissue_category_term].copy()
+for eqtl_id in sorted(df2['eqtl_id'].unique()):
+    print(eqtl_id)
+    cell_df = df2.loc[df2['eqtl_id'] == eqtl_id].copy()
     cell_df.drop(df.columns.tolist(), axis=1, inplace=True)
-    cell_df['etissue_category_term'] = etissue_category_term
+    cell_df['etissue_category_term'] = eqtl_id
 
-    track_config_str = """browser position chr5:132239646-132497907
-browser pack GWAS/eQTL colocalization
-track type=interact name="coloc_gwas_eqtl_{etissue_category_term}" description="An interact file" interactDirectional=true maxHeightPixels=200:100:50 visibility=full
-""".format(etissue_category_term=etissue_category_term)
-    cell_track_tsv = os.path.join(outdir_path, "{}.tsv".format(etissue_category_term.replace(' ', '_')))
-    with open(cell_track_tsv, 'w') as fout:
-        fout.write(track_config_str)
+#     track_config_str = """browser position chr5:132239646-132497907
+# browser pack GWAS/eQTL colocalization
+# track type=interact name="coloc_gwas_eqtl_{eqtl_id}" description="An interact file" interactDirectional=true maxHeightPixels=200:100:50 visibility=full
+# """.format(eqtl_id=eqtl_id)
+    cell_track_tsv = os.path.join(outdir_path, "{}.inter.bed".format(eqtl_id.replace(' ', '_')))
+    # with open(cell_track_tsv, 'w') as fout:
+    #     fout.write(track_config_str)
 
     cell_df = cell_df[['#chrom', 'chromStart', 'chromEnd', 'name', 'score', 'value', 'exp',
                'color', 'sourceChrom', 'sourceStart', 'sourceEnd', 'sourceName', 'sourceStrand',
                'targetChrom', 'targetStart', 'targetEnd', 'targetName', 'targetStrand']]
-    cell_df.to_csv(cell_track_tsv, sep='\t', header=True, mode='a', index=False)
+    cell_df.sort_values(by=cell_df.columns.tolist(), inplace=True)
+    # import pdb; pdb.set_trace()
+    cell_df.to_csv(cell_track_tsv, sep='\t', header=True, index=False)
